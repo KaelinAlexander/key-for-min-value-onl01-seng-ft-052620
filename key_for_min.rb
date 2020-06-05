4c1cbb8d5b2433beb836a2e1fe8ba401d692dd77
Kaelin Alexander <kaelin.alexander@gmail.com>
2020-06-05 14:44:36 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -6,11 +6,22 @@ def key_for_min_value(name_hash)
   name_hash.each do |key, value|
     if least == nil 
       least = value
+<<<<<<< HEAD
       least_key = key
+=======
+>>>>>>> 766c591527de35b9004ad022ec1fa49298ed8864
     elsif value < least
     least = value
     least_key = key
   end
 end
+<<<<<<< HEAD
     least_key
+=======
+  if name_hash={}
+    nil
+   else
+      least_key
+  end
+>>>>>>> 766c591527de35b9004ad022ec1fa49298ed8864
 end
